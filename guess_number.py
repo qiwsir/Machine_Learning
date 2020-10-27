b73500abe5177ebb68c4fea5522c4c8ed3bd14e8
陆丹妮 <cheneylu@promote.cache-dns.local>
2020-10-27 16:45:06 +0800

    项目上传

--- guess_number.py
+++ guess_number.py
@@ -17,35 +17,36 @@ def guess_number_game(guess_num,guess_chance):
     # 提示开始游戏
     print("Guess the number game begins(1-100):")
     # 循环开始，猜的数字在规定的范围内
-    for i in range(1,guess_chance + 1):
-        # 设置异常，如果输入的不是整数，则跳出异常
+    while sum < guess_chance:
+    #for i in range(0,guess_chance):
+        sum += 1
         try:
             guess = int(input("Please enter the number:"))
+        # 如果猜的数据不在规定范围内，则提示错误，并将该次猜测数记录进去
+            if guess < 0 or guess >= 100:
+                print("You must guess a number between 0-99!")
+
+        # 如果猜的数据小于设定数据，则输出数据偏小，并使得猜测次数+1
+            elif guess < guess_num:
+                print("Your guess is small！")
+
+        # 如果猜的数据大于设定数据，则输出数据偏大，并使得猜测次数+1
+            elif guess > guess_num:
+                print("Your guess is big！")
+
+        # 如果猜的数据等于设定数据，则输出"你猜对了"，并使得猜测次数+1
+            elif guess == guess_num:
+                print("Your are right!")
+                print("You have guessed " + str(sum) + " times！")
+                break
+        # 设置异常，如果输入的不是整数，则跳出异常
         except ValueError:
             print("Input Error")
             continue
-        # 如果猜的数据不在规定范围内，则提示错误，并将该次猜测数记录进去
-        if guess < 0 or guess >= 100:
-            print("You must guess a number between 0-99!")
-            sum += 1
-        # 如果猜的数据小于设定数据，则输出数据偏小，并使得猜测次数+1
-        elif guess < guess_num:
-            print("Your guess is small！")
-            sum += 1
-        # 如果猜的数据大于设定数据，则输出数据偏大，并使得猜测次数+1
-        elif guess > guess_num:
-            print("Your guess is big！")
-            sum += 1
-        # 如果猜的数据等于设定数据，则输出"你猜对了"，并使得猜测次数+1
-        elif guess == guess_num:
-            print("Your are right!")
-            sum += 1
-            print("You have guessed " + str(sum) + " times！")
-            break
     #如果猜测次数超过限定次数，则输出正确答案
-    while (guess_chance - sum) == 0:
+    if  sum == guess_chance:
         print("You have guessed "+ str(sum) + " times! The correct answer is : " + str(guess_num) + "!")
-        break
+
 #调用函数
 guess_number_game(guess_num,guess_chance)
 
