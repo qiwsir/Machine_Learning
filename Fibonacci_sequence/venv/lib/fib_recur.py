5acf12c3bbb6f77e9b0d12fbf64adbdc8ed63f21
陆丹妮 <cheneylu@promote.cache-dns.local>
2020-10-30 18:42:41 +0800

    项目上传

---
+++ Fibonacci_sequence/venv/lib/fib_recur.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python 
+# coding:utf-8 
+# @Time :2020/10/28 3:51 下午
+# @File :fib_recur.py
+
+# 定义一个用递归方法进行循环生成数列的函数
+def fib_recur_method(n):
+    if n <= 1:  # 第0项和第1项是它本身
+        return n
+    else:  # 反之则返回前两个数的和
+        return fib_recur_method(n - 1) + fib_recur_method(n - 2)
+
+# 将整个数列以列表的形式输出
+def fib_recur(n):
+    recur_result = []
+    for i in range(0, n + 1):  # 斐波那契数列是从第0项开始的
+        recur_result.append(fib_recur_method(i))
+    return recur_result
+
+if __name__ == "__main__":
+    n = int(input("Please enter an integer:"))
+    fib = fib_recur(n)
+    print(fib)
